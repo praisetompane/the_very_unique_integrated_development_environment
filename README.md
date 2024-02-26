b6369979f3cea6c679c6bd192e70b8aa1a95fd86
Praise Tompane <praisetompane@icloud.com>
2024-02-26 20:20:48 +0200

    fix(test_code): remove main entrty test code forogtten during refactor
    --date=0.day.ago

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# the_unique_integrated_development_environment
-Messing around with some barebones constructs required in an IDE
+# play_IDE
+messing around with some barebones constructs required in an IDE
+
+## setup instructions:
+- install `python 3.11` or higher.
+    - [Python Download](https://www.python.org/downloads/)
+- clone repo: 
+    ```shell
+    git clone git@github.com:praisetompane/play_IDE.git
+    ```
+## run program:
+
+- to start system run:
+    ```shell
+    python main.py
+    ```
--- app/ide.py
+++ app/ide.py
@@ -33,36 +33,3 @@ class IDE:
 
     def redo(self):
         self._text_editor.redo()
-
-
-def main():
-    current_document = ""
-    ide = IDE(current_document)
-
-    ide.write("public void add(int index, int element){")
-    ide.write("typo")
-    ide.undo()
-    ide.write(
-        """
-                if(numberOfElements < _size)
-                    array[index] = element;
-    """
-    )
-    ide.write(
-        """
-                else {
-                    resize();
-                    array[index] = element;
-                }
-    """
-    )
-    ide.write(
-        """
-            numberOfElements++;
-        }
-    """
-    )
-
-
-if __name__ == "__main__":
-    main()
